26dc3ecdaf724b61dfcaea766d3dd3c80692ab36
mahyarsadeghi <mahyarsadeghi.ga@gmail.com>
2024-11-25 23:27:04 -0500

    d

--- torchgfn/src/gfn/utils/evaluation.py
+++ torchgfn/src/gfn/utils/evaluation.py
@@ -8,7 +8,7 @@ import numpy as np
 from scipy.stats import spearmanr
 from tqdm import tqdm
 
-from gfn.gflownet import GFlowNet,FMGFlowNet
+from gfn.gflownet import GFlowNet, FMGFlowNet
 from gfn.gym import HyperGrid
 from gfn.modules import DiscretePolicyEstimator
 from gfn.samplers import Sampler
@@ -22,7 +22,9 @@ def timer(func):
         end_time = time.time()  # End the timer
         print(f"Function '{func.__name__}' executed in {end_time - start_time:.4f} seconds")
         return result
+
     return wrapper
+
 
 class TensorDict:
     def __init__(self, default_factory=None):
@@ -76,7 +78,7 @@ def get_all_transition_log_probs(env: DiscreteStates, pf_estimator):
     estimator_output = pf_estimator(all_states)
     dist = pf_estimator.to_probability_distribution(all_states, estimator_output)
     transition_log_probs = [None] * env.n_actions
-    
+
     for i in range(env.n_actions):
         action = torch.Tensor([i])
         transition_log_probs[i] = dist.log_prob(action)
@@ -96,7 +98,7 @@ def compute_log_probability(env: DiscreteStates, gfn, state: DiscreteStates, mem
     Returns:
         torch.Tensor: The log probability π_θ(s).
     """
-    if len(state.tensor.shape)==1:
+    if len(state.tensor.shape) == 1:
         state = stack_states([state])
     # Check if the result is already computed and stored in memo
     if state.tensor in memo:
@@ -107,12 +109,12 @@ def compute_log_probability(env: DiscreteStates, gfn, state: DiscreteStates, mem
         log_prob = torch.tensor([0.0], requires_grad=False)
         memo[state.tensor] = log_prob
         return log_prob
-    
+
     # Recursive case: compute log π_θ(s) from all parent states
     # Collect log-probabilities for each parent transition
     log_probs = []
     # to iterate each parent state and the corresponding action
-    for i in range(env.n_actions-1):
+    for i in range(env.n_actions - 1):
         action = env.actions_from_tensor(torch.Tensor([[i]]).to(torch.int64))
         env.update_masks(state)
         if env.is_action_valid(state, action, backward=True):
@@ -124,7 +126,7 @@ def compute_log_probability(env: DiscreteStates, gfn, state: DiscreteStates, mem
             # logPF(s|s'): Forward transition probability in log form
             log_forward_prob = transition_log_probs[i][parent_state_idx]
             # log π_θ(s'): Recursively compute log π_θ(parent_state)
-            log_parent_prob=compute_log_probability(env, gfn, parent_state, memo, transition_log_probs)
+            log_parent_prob = compute_log_probability(env, gfn, parent_state, memo, transition_log_probs)
             # Compute the sum inside the exponent for this parent
             log_probs.append(log_forward_prob + log_parent_prob)
     # Sum of exponentiated log-probabilities (log-sum-exp trick for numerical stability)
@@ -133,16 +135,19 @@ def compute_log_probability(env: DiscreteStates, gfn, state: DiscreteStates, mem
     memo[state.tensor] = log_prob
     return log_prob
 
+
 def compute_log_prob_termination(env: DiscreteStates, terminal_state: DiscreteStates, memo, transition_log_probs):
-    if len(terminal_state.tensor.shape)==1:
+    if len(terminal_state.tensor.shape) == 1:
         terminal_state = stack_states([terminal_state])
     terminal_state_tensor = terminal_state.tensor
-    termination_action = env.actions_from_tensor(torch.Tensor([[env.n_actions-1]]).to(torch.int64))
+    termination_action = env.actions_from_tensor(torch.Tensor([[env.n_actions - 1]]).to(torch.int64))
     env.update_masks(terminal_state)
-    assert env.is_action_valid(terminal_state, termination_action, backward=False), f"Error: Termination at given state {terminal_state.tensor} is invalid!"
+    assert env.is_action_valid(terminal_state, termination_action,
+                               backward=False), f"Error: Termination at given state {terminal_state.tensor} is invalid!"
     terminal_state_idx = env.get_states_indices(terminal_state)
     # log π_θ(s_terminal) + log termination
     return memo[terminal_state_tensor] + transition_log_probs[-1][terminal_state_idx]
+
 
 # to compute the sampling probability wit monte_carlo
 def count_occurrences_with_monte_carlo(env: DiscreteStates, sampler, n_samples=10000, show_progress=True):
@@ -164,13 +169,14 @@ def count_occurrences_with_monte_carlo(env: DiscreteStates, sampler, n_samples=1
     terminal_states = [traj.states[-2] for traj in trajectories]
     occurrences = TensorDict(int)
     for state in tqdm(terminal_states, desc="Processing terminal_states") if show_progress else terminal_states:
-        occurrences[state.tensor]+=1 
+        occurrences[state.tensor] += 1
     return occurrences
+
 
 def compute_log_prob_with_monte_carlo(occurrences, terminal_state, n_samples: int) -> float:
     # Calculate the probability
     if isinstance(terminal_state, States):
-        if len(terminal_state.tensor.shape)==1:
+        if len(terminal_state.tensor.shape) == 1:
             terminal_state = stack_states([terminal_state])
         terminal_state = terminal_state.tensor
     return torch.log(torch.tensor(occurrences[terminal_state] / n_samples, requires_grad=False))
@@ -182,6 +188,7 @@ def get_random_test_set(env: HyperGrid, n=100):
     log_rewards = torch.log(env.reward(terminal_states))
     return terminal_states, log_rewards
 
+
 def get_sampled_test_set(gfn, env, n=100):
     # sampler = Sampler(estimator=gfn.pf)
     # test_trajectories = sampler.sample_trajectories(env=env, n=n)
@@ -189,6 +196,7 @@ def get_sampled_test_set(gfn, env, n=100):
     terminal_states = gfn.sample_terminating_states(env=env, n=n)
     log_rewards = torch.log(env.reward(terminal_states))
     return terminal_states, log_rewards
+
 
 @timer
 def evaluate_GFNEvalS(gfn: GFlowNet, env: DiscreteStates, terminal_states, log_rewards):
@@ -214,24 +222,28 @@ def evaluate_GFNEvalS(gfn: GFlowNet, env: DiscreteStates, terminal_states, log_r
     # Calculate the log probability and log reward for each terminal state
     # for traj in test_trajectories:
     for terminal_state in tqdm(terminal_states, desc="Evaluating test set..."):
-        log_prob=compute_log_probability(env, gfn, terminal_state, memo, transition_log_probs)
+        log_prob = compute_log_probability(env, gfn, terminal_state, memo, transition_log_probs)
         log_probs.append(log_prob.detach().numpy())
-        log_prob_termination = compute_log_prob_termination(env, terminal_state, memo, transition_log_probs) 
+        log_prob_termination = compute_log_prob_termination(env, terminal_state, memo, transition_log_probs)
         log_probs_termination.append(log_prob_termination.detach().numpy())
     # 9 - Compute Spearman's Rank Correlation
     spearman_corr_termination, _ = spearmanr(log_probs_termination, log_rewards.detach())
-    print(f"Spearman's Rank Correlation (Modified GFNEvalS, including termination actions): {spearman_corr_termination}. Runtime: {time.time()-start_time} seconds.")
+    print(
+        f"Spearman's Rank Correlation (Modified GFNEvalS, including termination actions): {spearman_corr_termination}. Runtime: {time.time() - start_time} seconds.")
     return spearman_corr_termination, memo, transition_log_probs
 
+
 @timer
-def evaluate_GFNEvalS_with_monte_carlo(gfn: GFlowNet, env: DiscreteStates, 
-                                        terminal_states, log_rewards, sampler=None,
-                                        n_samples=80000,
-                                        show_progress: bool=True):
+def evaluate_GFNEvalS_with_monte_carlo(gfn: GFlowNet, key="TB", env: DiscreteStates,
+                                       terminal_states, log_rewards, sampler=None,
+                                       n_samples=80000,
+                                       show_progress: bool = True):
     """Computes the sampling probability of given terminal states using Monte Carlo.
 
     Args:
         gfn: An initialized Sampler using the forward policy estimator.
+        key: one of the following: "FM" (Flow Matching), "TB"(Trajectory Balance),
+    "SubTB"(Sub TB), "DB"(Detailed Balance).
         env: The HyperGrid environment instance.
         terminal_states: terminal states in test set.
         log_rewards: true rewards of terminal states in test set.
@@ -244,24 +256,22 @@ def evaluate_GFNEvalS_with_monte_carlo(gfn: GFlowNet, env: DiscreteStates,
     """
     start_time = time.time()
 
-    sampler=None
-    print("Gfn Type is : ", gfn)
-    if gfn is FMGFlowNet:
-        sampler=Sampler(estimator=gfn.logF)
-    else:
+    if sampler is None:
         sampler = Sampler(estimator=gfn.pf)
     # Generate a large number of samples as monte carlo to count occurrences of appeared terminal states
     occurrences = count_occurrences_with_monte_carlo(env, sampler, n_samples=n_samples, show_progress=show_progress)
     #
     log_probs_monte_carlo = []
 
-    for terminal_state in tqdm(terminal_states, desc="Evaluating GFNEvalS with monte carlo") if show_progress else terminal_states:
+    for terminal_state in tqdm(terminal_states,
+                               desc="Evaluating GFNEvalS with monte carlo") if show_progress else terminal_states:
         log_prob = compute_log_prob_with_monte_carlo(occurrences, terminal_state, n_samples)
         log_probs_monte_carlo.append(log_prob.detach().numpy())
     # Compute Spearman's Rank Correlation
     spearman_corr_monte_carlo, _ = spearmanr(log_probs_monte_carlo, log_rewards)
     if show_progress:
-        print(f"Spearman's Rank Correlation (Monte Carlo): {spearman_corr_monte_carlo}. MC sample number: {n_samples}. Runtime: {time.time()-start_time} seconds")
+        print(
+            f"Spearman's Rank Correlation (Monte Carlo): {spearman_corr_monte_carlo}. MC sample number: {n_samples}. Runtime: {time.time() - start_time} seconds")
     return spearman_corr_monte_carlo, occurrences, log_probs_monte_carlo
 
 
@@ -278,6 +288,7 @@ class PhiFunction(nn.Module):
         x = self.relu(self.linear1(x))
         return self.linear2(x)
 
+
 def calc_KL_using_model(model, samples_p, samples_q, no_grad=False):
     # Compute f(x) for samples from P and Q
     if no_grad:
@@ -290,15 +301,17 @@ def calc_KL_using_model(model, samples_p, samples_q, no_grad=False):
 
     # Compute the terms of the formula
     term_p = torch.mean(f_p)  # Expectation over P: E_P[f]
-    term_q = torch.log(torch.mean(torch.exp(torch.clamp(f_q, max=695, min=-695))))  # Log of expectation over Q: log(E_Q[e^f])
+    term_q = torch.log(
+        torch.mean(torch.exp(torch.clamp(f_q, max=695, min=-695))))  # Log of expectation over Q: log(E_Q[e^f])
 
     # KL divergence
     kl_div = term_p - term_q
     return kl_div
 
-def compute_KL(p_star_sample : Tensor, p_hat_sample : Tensor,
-                layer_size=128, num_epochs=200, lr=0.001, show_progress=False, 
-                device='cuda'):
+
+def compute_KL(p_star_sample: Tensor, p_hat_sample: Tensor,
+               layer_size=128, num_epochs=200, lr=0.001, show_progress=False,
+               device='cuda'):
     # Ensure both samples have the same shape
     assert p_star_sample[0].shape == p_hat_sample[0].shape
     input_size = p_star_sample[0].numel()
